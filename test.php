916c502b6c34e96858dd2eb541927b9ba5097030
Kakip1919 <invest9me@yahoo.co.jp>
2021-12-08 21:26:11 +0900

    2nd

--- test.php
+++ test.php
@@ -1,17 +1,24 @@
 <?php
-// Guzzleを利用するためComposerのautoload.phpを読み込み
+
 require __DIR__ . '/vendor/autoload.php';
-// Clientクラスをインポート
+
 use GuzzleHttp\Client;
 
-// Clientクラスのインスタンスを作成
-$client = new Client();
+function get_api($id){
+    $client = new Client();
+    $api_result = $client->request('GET', 'https://api.liquid.com/products/'.$id);
+    $response = $api_result->getBody();
+    return json_decode($response);
+}
 
-// リクエストURLを指定
+function commission($coin): float
+{
+    $result_price = $coin * 0.001;
+    return $coin - $result_price;
+}
 
-//$response = $client->request('GET', 'https://api.liquid.com/products/5');
-//$res = $response->getBody();
-//$res = json_decode($res);
-//
-//echo $res->market_ask;
+// ETH->JPY - (BTC->JPY * ETH->BTC)
+$result_ETH = commission(get_api(29)->market_bid) - commission(get_api(5)->market_bid * get_api(37)->market_bid);
+
+echo $result_ETH;
 
